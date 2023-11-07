4dba8dd220e9c756da6e389e23d0d76bf1b7a504
Mathieu Sabourin <mathieu@sabourin.dev>
2023-11-07 16:31:18 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import MyHeader from '@/components/MyHeader.vue';
+</script>
+
+<template>
+  <MyHeader>
+  </MyHeader>
+
+    <div class="container">
+        <RouterView />
+    </div>
+</template>
+
+<style scoped>
+.container {
+    height: 100vh;
+    padding-top: 100px;
+    overflow: auto;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import MyTitle from '@/components/MyTitle';
+
+const isItalic = ref(false);
+
+const imageSrc = 'https://picsum.photos/550';
+const alt = 'my-random-image';
+
+const imgObjectAttributes = {
+  src: imageSrc,
+  alt
+}
+</script>
+
+<template>
+  <main>
+    <router-link :to="{name: 'about'}">A propos</router-link>
+
+  </main>
+</template>
+
+<style scoped>
+p {
+    & .italic {
+        font-style: italic;
+    }
+}
+img {
+    width: 500px;
+    max-height: 100%;
+}
+</style>
