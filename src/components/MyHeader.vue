370687b46d24acb31238017627908f317d56b7aa
Mathieu Sabourin <mathieu@sabourin.dev>
2023-11-14 15:25:48 +0100

    add velocity and plus transition hooks on it

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -7,7 +7,12 @@
     <div class="nav-wrapper" v-click-outside="() => showNav = false">
       <MyButton @click="showNav = !showNav">Menu</MyButton>
 
-      <transition name="slide-down">
+      <transition
+        @before-enter="onBeforeEnter"
+        @enter="onEnter"
+        @leave="onLeave"
+        :css="false"
+      >
         <nav v-show="showNav" class="nav">
           <ul>
             <li v-for="(item, index) in menuItems" :key="index">
@@ -23,13 +28,33 @@
 <script setup lang="ts">
 import { type Ref, ref } from 'vue';
 import MyButton from '@/components/MyButton.vue';
+import Velocity from 'velocity-animate';
 
 interface Props {
   menuItems: any[];
 }
+
 defineProps<Props>()
 
 const showNav: Ref<boolean> = ref(false);
+
+const onBeforeEnter = (el: HTMLElement) => {
+  el.style.right = '-150px';
+};
+const onEnter = (el: HTMLElement, done: () => void) => {
+  Velocity(
+    el,
+    { right: 0},
+    { duration: 500, easing: 'easeOutCubic', complete: done}
+  );
+};
+const onLeave = (el: HTMLElement, done: () => void) => {
+  Velocity(
+    el,
+    { right: '-150px'},
+    { duration: 250, easing: 'easeInCubic', complete: done}
+  );
+};
 </script>
 
 <style scoped>
@@ -44,13 +69,14 @@ header {
     position: relative;
 
     & .nav {
-      position: absolute;
+      position: fixed;
       right: 0;
+      top: 0;
       z-index: 1;
       background: var(--vt-c-indigo);
-      min-width: 150px;
+      width: 150px;
       padding: 10px;
-      border-radius: 5px;
+      height: 100vh;
 
       & ul {
         list-style: none;
